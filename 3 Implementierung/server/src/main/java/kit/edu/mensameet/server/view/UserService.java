2eba0e4fe31f9192d8c1295fb03696344ce578be
Kathrin Blum <unebi@student.kit.edu>
2019-07-08 20:43:04 +0200

    persistence with mysql is working now

--- 3 Implementierung/server/src/main/java/kit/edu/mensameet/server/view/UserService.java
+++ 3 Implementierung/server/src/main/java/kit/edu/mensameet/server/view/UserService.java
@@ -38,4 +38,12 @@ public class UserService {
       FirebaseAuth.getInstance().createUser(request);
      
     }
+    
+    @RequestMapping("/addTestUser")
+    User addUser() {
+    	User test = new User("hans");
+    	test.setToken("test");
+    	userController.addUser("testObject");
+    	return test; 
+    }
 }
