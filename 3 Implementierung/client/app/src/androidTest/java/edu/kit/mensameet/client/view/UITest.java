c0f14441350e0b399ab5a869aacb90e9563b3c6b
Matthias Bruns <matthiasbruns35@gmail.com>
2019-08-28 03:12:56 +0200

    first experimental espresso test

---
+++ 3 Implementierung/client/app/src/androidTest/java/edu/kit/mensameet/client/view/UITest.java
@@ -0,0 +1,36 @@
+package edu.kit.mensameet.client.view;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UITest {
+    @Rule
+    public ActivityTestRule<BeginActivity> activityRule =
+            new ActivityTestRule<>(BeginActivity.class);
+
+    @Test
+    public void espresso() {
+        onView(withId(R.id.goEat)).perform(click());
+        onView(withText("Hello Steve!")).check(matches(isDisplayed()));
+    }
+}
