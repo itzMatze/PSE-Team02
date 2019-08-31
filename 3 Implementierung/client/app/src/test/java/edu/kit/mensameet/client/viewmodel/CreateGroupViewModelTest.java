395a8c3bdabbcb9c9cc7a3bda49b4c2936b0ac29
Fangzhou Bian <bianfangzhou@gmail.com>
2019-08-31 10:39:18 +0200

    added ViewModelTests

--- 3 Implementierung/client/app/src/test/java/edu/kit/mensameet/client/viewmodel/CreateGroupViewModelTest.java
+++ 3 Implementierung/client/app/src/test/java/edu/kit/mensameet/client/viewmodel/CreateGroupViewModelTest.java
@@ -2,30 +2,20 @@ package edu.kit.mensameet.client.viewmodel;
 
 import android.util.Pair;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
-
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import edu.kit.mensameet.client.model.Group;
 import edu.kit.mensameet.client.model.MensaMeetSession;
 import edu.kit.mensameet.client.model.User;
 import edu.kit.mensameet.client.util.RequestUtil;
-import edu.kit.mensameet.client.view.BuildConfig;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.powermock.api.mockito.PowerMockito.doNothing;
